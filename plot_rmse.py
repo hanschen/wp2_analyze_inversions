9119101795dcc59b7a445cf5f08441c54861cdde
Hans Chen <contact@hanschen.org>
2026-03-21 18:30:36 +0100

    Change to RER, add date formatting, etc.

--- plot_rmse.py
+++ plot_rmse.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run --script
 """Plot posterior flux and parameter RMSE relative to prior over time."""
 
 from datetime import datetime
 
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import netCDF4 as nc
 import numpy as np
@@ -11,8 +12,8 @@ import seaborn as sns
 sns.set_theme("paper", "ticks")
 sns.set_color_codes()
 
-# CASE = "summer"
-CASE = "winter"
+CASE = "summer"
+# CASE = "winter"
 
 CYCLE_START = {
     "summer": datetime(2021, 6, 17, 0, 0, 0),
@@ -22,6 +23,16 @@ CYCLE_START = {
 SPINUP_END = {
     "summer": datetime(2021, 7, 1, 0, 0, 0),
     "winter": datetime(2021, 2, 1, 0, 0, 0),
+}[CASE]
+
+ANNOTATION = {
+    "summer": "a",
+    "winter": "b",
+}[CASE]
+
+SHOW_LEGEND = {
+    "summer": False,
+    "winter": True,
 }[CASE]
 
 
@@ -60,6 +71,10 @@ def hourly2daily(y):
 
 def rmse(y, y_truth):
     return np.sqrt(np.nanmean((y - y_truth) ** 2, axis=(-1, -2)))
+
+
+def rer(rmse_post, rmse_prior):
+    return (1 - rmse_post / rmse_prior) * 100
 
 
 # %% Load data
@@ -108,22 +123,33 @@ parameters_daily_truth = hourly2daily(parameters_truth)
 parameters_daily_prior = hourly2daily(parameters_prior)
 parameters_daily_post = hourly2daily(parameters_post)
 
-# Calculate RMSEs
+# Calculate RMSEs and RERs
 rmse_prior = rmse(fluxes_daily_prior, fluxes_daily_truth)
 rmse_post = rmse(fluxes_daily_post, fluxes_daily_truth)
 
 rmse_prior_parameters = rmse(parameters_daily_prior, parameters_daily_truth)
 rmse_post_parameters = rmse(parameters_daily_post, parameters_daily_truth)
 
+rer_fluxes = rer(rmse_post, rmse_prior)
+rer_parameters = rer(rmse_post_parameters, rmse_prior_parameters)
+
+start = np.nonzero(time_daily == SPINUP_END)[0].item()
+rer_fluxes_mean = rer_fluxes[start:].mean()
+rer_parameters_mean = rer_parameters[start:].mean()
+
+print(":: RER")
+print(f"-> fluxes: {rer_fluxes_mean:.1f}%")
+print(f"-> parameters: {rer_parameters_mean:.1f}%")
+
 
 # %% Plot
 
-fig = plt.figure(figsize=(8, 6))
+fig = plt.figure(figsize=(3.5, 3))
 ax = fig.add_subplot(111)
 
 ax.plot(
     time_daily,
-    rmse_post / rmse_prior,
+    rer_fluxes,
     color="g",
     linewidth=1.5,
     label="Fluxes",
@@ -131,7 +157,7 @@ ax.plot(
 
 ax.plot(
     time_daily,
-    rmse_post_parameters / rmse_prior_parameters,
+    rer_parameters,
     color="b",
     linewidth=1.5,
     label="Parameters",
@@ -139,20 +165,57 @@ ax.plot(
 
 ax.fill_between(
     [CYCLE_START, SPINUP_END],
-    [0, 0],
-    [1, 1],
+    0,
+    1,
     facecolor="0.9",
     edgecolor="0.8",
     hatch="/",
-    label="Spin-up",
+    transform=ax.get_xaxis_transform(),
+    # label="Spinup",
 )
 
-ax.set_xticks(time_daily[::7])
-ax.set_xlim(CYCLE_START, time_daily[-1])
-ax.set_ylim(0, 1)
+ax.text(
+    0.04,
+    0.96,
+    ANNOTATION,
+    color="k",
+    fontsize=12,
+    ha="left",
+    va="top",
+    bbox=dict(facecolor="w", edgecolor="k"),
+    transform=ax.transAxes,
+)
 
-ax.legend(loc="upper right")
+# Mean error reduction
+# ax.plot(
+#     [SPINUP_END, time_daily[-1]],
+#     [rer_fluxes_mean, rer_fluxes_mean],
+#     color="g",
+#     linewidth=1.5,
+#     linestyle="--",
+# )
+
+# ax.plot(
+#     [SPINUP_END, time_daily[-1]],
+#     [rer_parameters_mean, rer_parameters_mean],
+#     color="b",
+#     linewidth=1.5,
+#     linestyle="--",
+# )
+
+ax.set_xticks(time_daily[::7])
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+# for label in ax.get_xticklabels(which="major"):
+#     label.set(rotation=30, horizontalalignment="right")
+ax.set_xlim(CYCLE_START, time_daily[-1])
+ax.set_ylim(0, 100)
+
+if SHOW_LEGEND:
+    ax.legend(loc="upper right")
 ax.set_xlabel("Date")
-ax.set_ylabel("Relative RMSE")
+ax.set_ylabel("Relative error reduction (%)")
+
+fig.tight_layout()
+fig.savefig(f"output/rmse_{CASE}.png")
 
 plt.show()
